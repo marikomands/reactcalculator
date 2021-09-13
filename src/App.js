2db68ad437f544fcfa938f6a5a4750551f35f25d
marikomands <marikomands@gmail.com>
2021-09-13 13:59:30 +0100

    calculator framwork

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Calculator</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc-app">
+      <form>
+        <input type="text" />
+      </form>
+      <div classname="keypad">
+        <div className="container">
+          <button>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button> /</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>×</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button id="zero">0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
